aea55e92b41c6d953d1738d80808de4bd2cc7ba4
ThreeHDM <juanbilardi@gmail.com>
2020-05-09 11:51:36 -0300

    se avanza en script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
+// El tablero vacío
 var origBoard;
+
+// El Human Player
 const huPlayer = '0';
+
+// AI Player
 const aiPlayer = 'X';
+
+// Combos ganadores
 const winCombos = [
     [0, 1, 2],
     [3, 4, 5],
@@ -12,16 +19,19 @@ const winCombos = [
     [6, 4, 2]
 ]
 
+
 const cells = document.querySelectorAll('.cell')
 
 startGame();
 
 function startGame(){
     // We hide the endgame message
-    document.querySelector(".endgame").getElementsByClassName.display = "none"
+    document.querySelector(".endgame").style.display = "none"
     
+    // Crea un array del 0 al 9
     origBoard = Array.from(Array(9).keys())
     
+    //Recorre los .cells
     for(var i = 0; i < cells.length; i++){
         cells[i].innerText = '';
         cells[i].style.removeProperty('background-color');
@@ -31,5 +41,58 @@ function startGame(){
 }
 
 function turnClick(square) {
-    console.log(square.target.id)
+    turn(square.target.id, huPlayer)
 }
+
+function turn(squareId, player){
+    // Le pasa el símbolo del jugador al cuadrado
+    origBoard[squareId] = player;
+    document.getElementById(squareId).innerText = player;
+    let gameWon = checkWin(origBoard, player)
+    if(gameWon) {
+        gameOver(gameWon)
+    } 
+}
+
+function checkWin(board, player) {
+
+    let plays = board.reduce((a, e, i) => (e === player)) ? a.concat(i) : a, []);
+    let gameWon = null;
+    for (let [index, win] of winCombos.entries()) {
+        if (win.every(elem = plays.indexOf(elem > -1)) {
+            gameWon = {index: index, player: player};
+            break;
+        });
+        return gameWon;
+    }
+}
+/*
+Arrow Function
+const add = (a, b) => (a + b)
+
+Is equivalent to
+
+const add = (a, b) => {
+    return a + b;
+}
+
+When you use the() after your => it just automatically returns the values inside.
+
+const moli = (x, y) => (x+y);
+
+console.log(moli(1,2) )
+*/
+
+/*
+Ternary operator
+
+var foo;
+if(x){
+    foo = algo
+} else {
+    foo = otra
+}
+
+var foo = (x) ?  "valor verdadero" : "valor falso";
+
+*/
